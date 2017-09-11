2ce21a1088b98b3cfd104e5d400d012c9e617552
Igor Chernyy <igor.chernyy@Igors-MacBook-Pro-2.local>
2017-09-11 11:59:43 -0400

    clean up

--- test/test_batchPut.js
+++ test/test_batchPut.js
@@ -6,39 +6,26 @@ describe('BatchPut', () => {
   const tableName = 'testTable';
   const testItem1 = {
     test: 'abc',
-    number: 123456789,
+    number: 123456789
   };
   const testItem2 = {
     test: 'abc-1',
     number: 123456789,
-    list: ['a', 'b', 'c'],
+    list: ['a', 'b', 'c']
   };
 
-  const expPayload = {
-    RequestItems: {},
-  };
-  expPayload.RequestItems[tableName] = [{
-      PutRequest: {
-        Item: testItem1,
-      },
-    },
-    {
-      PutRequest: {
-        Item: testItem2,
-      },
-    },
-  ];
+  const expPayload = { RequestItems: {} };
+  expPayload.RequestItems[tableName] = [{ PutRequest: { Item: testItem1 } }, { PutRequest: { Item: testItem2 } }];
 
   it('success', () => {
     const db = new DynamoDB();
     const getStub = sinon.stub(db.docClient, 'batchWrite');
     getStub.callsArgWith(1, null);
 
-    return db.batchPut(tableName, [testItem1, testItem2])
-      .then(() => {
-        getStub.calledWith(expPayload);
-        assert(getStub.calledOnce);
-      });
+    return db.batchPut(tableName, [testItem1, testItem2]).then(() => {
+      getStub.calledWith(expPayload);
+      assert(getStub.calledOnce);
+    });
   });
 
   it('error', () => {
@@ -47,11 +34,12 @@ describe('BatchPut', () => {
     const getStub = sinon.stub(db.docClient, 'batchWrite');
     getStub.callsArgWith(1, awsError, null);
 
-    return db.batchPut(tableName, [testItem1, testItem2])
+    return db
+      .batchPut(tableName, [testItem1, testItem2])
       .then(() => {
         throw new Error('Expected error to be  thrown');
       })
-      .catch((err) => {
+      .catch(err => {
         assert(getStub.calledOnce);
         getStub.calledWith(expPayload);
         assert.equal(err, awsError);
